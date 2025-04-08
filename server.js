c259431242af5c77312fef27a829f0e5d0c42e68
Olakareem <olakareemomobolarinwa@gmail.com>
2025-04-08 06:03:12 +0100

    first commit

--- server.js
+++ server.js
@@ -9,8 +9,13 @@ require("dotenv/config");
 const app = express();
 
 // Middleware
-app.use(cors());
-app.use(express.json());
+app.use(
+  cors({
+    origin: "*", // Allow all origins
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allow these HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allow these headers
+  })
+);app.use(express.json());
 
 // Connect to MongoDB
 connectDB().catch((error) => {
